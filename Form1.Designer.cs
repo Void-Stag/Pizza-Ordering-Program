dcd386320c1eda948932fa4d36430f31e2bad101
Void-Stag <64465757+Void-Stag@users.noreply.github.com>
2022-10-14 19:09:28 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,397 @@
+﻿namespace Assignment_2_PIzza_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstOrderreveiw = new System.Windows.Forms.ListBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.cboStarter = new System.Windows.Forms.ComboBox();
+            this.cboBase = new System.Windows.Forms.ComboBox();
+            this.cboSweet = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ckbPufferfish = new System.Windows.Forms.CheckBox();
+            this.ckbSquid = new System.Windows.Forms.CheckBox();
+            this.ckbPepperoni = new System.Windows.Forms.CheckBox();
+            this.ckbCheese = new System.Windows.Forms.CheckBox();
+            this.ckbOlives = new System.Windows.Forms.CheckBox();
+            this.ckbPeppers = new System.Windows.Forms.CheckBox();
+            this.ckbHam = new System.Windows.Forms.CheckBox();
+            this.ckbPineapple = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstOrderreveiw
+            // 
+            this.lstOrderreveiw.FormattingEnabled = true;
+            this.lstOrderreveiw.Location = new System.Drawing.Point(588, 55);
+            this.lstOrderreveiw.Name = "lstOrderreveiw";
+            this.lstOrderreveiw.Size = new System.Drawing.Size(200, 212);
+            this.lstOrderreveiw.TabIndex = 4;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(525, 373);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(343, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Order Pizza";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Choose a Starter:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Choose a Base:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(345, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Select a Sweet:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(336, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Select Toppings:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(109, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 14;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 21);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 15;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 64);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 16;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // cboStarter
+            // 
+            this.cboStarter.FormattingEnabled = true;
+            this.cboStarter.Items.AddRange(new object[] {
+            "Olives",
+            "Garlic Bread",
+            "Dough Balls"});
+            this.cboStarter.Location = new System.Drawing.Point(107, 103);
+            this.cboStarter.Name = "cboStarter";
+            this.cboStarter.Size = new System.Drawing.Size(116, 21);
+            this.cboStarter.TabIndex = 14;
+            this.cboStarter.SelectedIndexChanged += new System.EventHandler(this.CboStarter_SelectedIndexChanged);
+            // 
+            // cboBase
+            // 
+            this.cboBase.FormattingEnabled = true;
+            this.cboBase.Items.AddRange(new object[] {
+            "Stuffed Crust",
+            "Thin Crust",
+            "Vegetarian"});
+            this.cboBase.Location = new System.Drawing.Point(107, 265);
+            this.cboBase.Name = "cboBase";
+            this.cboBase.Size = new System.Drawing.Size(116, 21);
+            this.cboBase.TabIndex = 15;
+            this.cboBase.SelectedIndexChanged += new System.EventHandler(this.CboBase_SelectedIndexChanged);
+            // 
+            // cboSweet
+            // 
+            this.cboSweet.FormattingEnabled = true;
+            this.cboSweet.Items.AddRange(new object[] {
+            "Marshmallow",
+            "Hershleys Chocolate",
+            "PEZ",
+            "Strawberry Laces"});
+            this.cboSweet.Location = new System.Drawing.Point(417, 265);
+            this.cboSweet.Name = "cboSweet";
+            this.cboSweet.Size = new System.Drawing.Size(165, 21);
+            this.cboSweet.TabIndex = 16;
+            this.cboSweet.SelectedIndexChanged += new System.EventHandler(this.CboSweet_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ckbPufferfish);
+            this.groupBox1.Controls.Add(this.ckbSquid);
+            this.groupBox1.Controls.Add(this.ckbPepperoni);
+            this.groupBox1.Controls.Add(this.ckbCheese);
+            this.groupBox1.Controls.Add(this.ckbOlives);
+            this.groupBox1.Controls.Add(this.ckbPeppers);
+            this.groupBox1.Controls.Add(this.ckbHam);
+            this.groupBox1.Controls.Add(this.ckbPineapple);
+            this.groupBox1.Location = new System.Drawing.Point(374, 110);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(182, 116);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            // 
+            // ckbPufferfish
+            // 
+            this.ckbPufferfish.AutoSize = true;
+            this.ckbPufferfish.Location = new System.Drawing.Point(92, 88);
+            this.ckbPufferfish.Name = "ckbPufferfish";
+            this.ckbPufferfish.Size = new System.Drawing.Size(70, 17);
+            this.ckbPufferfish.TabIndex = 25;
+            this.ckbPufferfish.Text = "Pufferfish";
+            this.ckbPufferfish.UseVisualStyleBackColor = true;
+            this.ckbPufferfish.CheckedChanged += new System.EventHandler(this.CkbPufferfish_CheckedChanged);
+            // 
+            // ckbSquid
+            // 
+            this.ckbSquid.AutoSize = true;
+            this.ckbSquid.Location = new System.Drawing.Point(6, 88);
+            this.ckbSquid.Name = "ckbSquid";
+            this.ckbSquid.Size = new System.Drawing.Size(53, 17);
+            this.ckbSquid.TabIndex = 24;
+            this.ckbSquid.Text = "Squid";
+            this.ckbSquid.UseVisualStyleBackColor = true;
+            this.ckbSquid.CheckedChanged += new System.EventHandler(this.CkbSquid_CheckedChanged);
+            // 
+            // ckbPepperoni
+            // 
+            this.ckbPepperoni.AutoSize = true;
+            this.ckbPepperoni.Location = new System.Drawing.Point(92, 65);
+            this.ckbPepperoni.Name = "ckbPepperoni";
+            this.ckbPepperoni.Size = new System.Drawing.Size(74, 17);
+            this.ckbPepperoni.TabIndex = 23;
+            this.ckbPepperoni.Text = "Pepperoni";
+            this.ckbPepperoni.UseVisualStyleBackColor = true;
+            this.ckbPepperoni.CheckedChanged += new System.EventHandler(this.CkbPepperoni_CheckedChanged);
+            // 
+            // ckbCheese
+            // 
+            this.ckbCheese.AutoSize = true;
+            this.ckbCheese.Location = new System.Drawing.Point(6, 65);
+            this.ckbCheese.Name = "ckbCheese";
+            this.ckbCheese.Size = new System.Drawing.Size(62, 17);
+            this.ckbCheese.TabIndex = 22;
+            this.ckbCheese.Text = "Cheese";
+            this.ckbCheese.UseVisualStyleBackColor = true;
+            this.ckbCheese.CheckedChanged += new System.EventHandler(this.CkbCheese_CheckedChanged);
+            // 
+            // ckbOlives
+            // 
+            this.ckbOlives.AutoSize = true;
+            this.ckbOlives.Location = new System.Drawing.Point(91, 42);
+            this.ckbOlives.Name = "ckbOlives";
+            this.ckbOlives.Size = new System.Drawing.Size(85, 17);
+            this.ckbOlives.TabIndex = 21;
+            this.ckbOlives.Text = "Black Olives";
+            this.ckbOlives.UseVisualStyleBackColor = true;
+            this.ckbOlives.CheckedChanged += new System.EventHandler(this.CkbOlives_CheckedChanged);
+            // 
+            // ckbPeppers
+            // 
+            this.ckbPeppers.AutoSize = true;
+            this.ckbPeppers.Location = new System.Drawing.Point(6, 42);
+            this.ckbPeppers.Name = "ckbPeppers";
+            this.ckbPeppers.Size = new System.Drawing.Size(65, 17);
+            this.ckbPeppers.TabIndex = 20;
+            this.ckbPeppers.Text = "Peppers";
+            this.ckbPeppers.UseVisualStyleBackColor = true;
+            this.ckbPeppers.CheckedChanged += new System.EventHandler(this.CkbPeppers_CheckedChanged);
+            // 
+            // ckbHam
+            // 
+            this.ckbHam.AutoSize = true;
+            this.ckbHam.Location = new System.Drawing.Point(91, 19);
+            this.ckbHam.Name = "ckbHam";
+            this.ckbHam.Size = new System.Drawing.Size(48, 17);
+            this.ckbHam.TabIndex = 19;
+            this.ckbHam.Text = "Ham";
+            this.ckbHam.UseVisualStyleBackColor = true;
+            this.ckbHam.CheckedChanged += new System.EventHandler(this.CkbHam_CheckedChanged);
+            // 
+            // ckbPineapple
+            // 
+            this.ckbPineapple.AutoSize = true;
+            this.ckbPineapple.Location = new System.Drawing.Point(6, 19);
+            this.ckbPineapple.Name = "ckbPineapple";
+            this.ckbPineapple.Size = new System.Drawing.Size(73, 17);
+            this.ckbPineapple.TabIndex = 18;
+            this.ckbPineapple.Text = "Pineapple";
+            this.ckbPineapple.UseVisualStyleBackColor = true;
+            this.ckbPineapple.CheckedChanged += new System.EventHandler(this.CkbPineapple_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(659, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(647, 328);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(0, 13);
+            this.lblPrice.TabIndex = 19;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(707, 273);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 20;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(597, 273);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 21;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 408);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cboSweet);
+            this.Controls.Add(this.cboBase);
+            this.Controls.Add(this.cboStarter);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lstOrderreveiw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox lstOrderreveiw;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.ComboBox cboStarter;
+        private System.Windows.Forms.ComboBox cboBase;
+        private System.Windows.Forms.ComboBox cboSweet;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox ckbPufferfish;
+        private System.Windows.Forms.CheckBox ckbSquid;
+        private System.Windows.Forms.CheckBox ckbPepperoni;
+        private System.Windows.Forms.CheckBox ckbCheese;
+        private System.Windows.Forms.CheckBox ckbOlives;
+        private System.Windows.Forms.CheckBox ckbPeppers;
+        private System.Windows.Forms.CheckBox ckbHam;
+        private System.Windows.Forms.CheckBox ckbPineapple;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_2_PIzza_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            myToolTip();
+        }
+        private void myToolTip()
+        {
+            ToolTip myToolTip = new ToolTip();
+            myToolTip.SetToolTip(cboStarter, "Choose from arange of starters");//Displays text in quotation marks when hovering over combo box
+            myToolTip.SetToolTip(button1, " in £");//Displays text in quotation marks when hovering over button
+        }
+
+        private void CboStarter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //I've done this so I don't need to
+       //   lstOrderreveiw.Items.Add(cboStarter.SelectedItem);//Adds Selected item from Combobox to Listbox
+
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            ckbPineapple.Checked = false;//Unchecks the Checkbox
+            ckbPeppers.Checked = false;//Unchecks the Checkbox
+            ckbCheese.Checked = false;//Unchecks the Checkbox
+            ckbSquid.Checked = false;//Unchecks the Checkbox
+            ckbHam.Checked = false;//Unchecks the Checkbox
+            ckbOlives.Checked = false;//Unchecks the Checkbox
+            ckbPepperoni.Checked = false;//Unchecks the Checkbox
+            ckbPufferfish.Checked = false;//Unchecks the Checkbox
+            cboStarter.Text = String.Empty;//Empties combobox
+            cboBase.Text = String.Empty;//Empties combobox
+            cboSweet.Text = String.Empty;//Empties combobox
+            lstOrderreveiw.Items.Clear();//Clears Listbox
+            lblPrice.Text = ("");//Clears Label
+        }
+        private void CboBase_SelectedIndexChanged(object sender, EventArgs e)
+        //I've done this so it acts as some sort of Data Validation
+        {
+         // lstOrderreveiw.Items.Add(cboBase.SelectedItem);//Adds Selected item from Combobox to Listbox
+        }
+
+        private void CboSweet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         // lstOrderreveiw.Items.Add(cboSweet.SelectedItem);//Adds Selected item from Combobox to Listbox
+        }
+
+        private void CkbPineapple_CheckedChanged(object sender, EventArgs e)
+        //I've done this so it acts as some sort of Data Validation
+        {
+            if (ckbPineapple.Checked == true)//Checks if the checkbox is checked, before adding Pineapple to the listbox
+            {
+                lstOrderreveiw.Items.Add("Pineapple");//Adds item to listbox
+            }
+
+            else//Checks if the checkbox is unchecked, before removeing the item if its present in the listbox
+            {
+                lstOrderreveiw.Items.Remove("Pineapple");//Removes item from listbox
+            }
+        }
+
+        private void CkbPeppers_CheckedChanged(object sender, EventArgs e)
+        //I've done this so it acts as some sort of Data Validation
+        {   //if the checkbox is checked, it will add Peppers to the listbox
+            if (ckbPeppers.Checked == true)
+            {
+                lstOrderreveiw.Items.Add("Peppers");
+            }
+            //if the checkbox is unchecked, it will remove the text above if it is present in the listbox
+            else
+            {
+                lstOrderreveiw.Items.Remove("Peppers");
+            }
+        }
+
+        private void CkbCheese_CheckedChanged(object sender, EventArgs e)
+        //I've done this so it acts as some sort of Data Validation
+        {   //if the checkbox is checked, it will add Cheese to the listbox
+            if (ckbCheese.Checked == true)
+            {
+                lstOrderreveiw.Items.Add("Cheese");
+            }
+            //if the checkbox is unchecked, it will remove the text above if it is present in the listbox
+            else
+            {
+                lstOrderreveiw.Items.Remove("Cheese");
+            }
+        }
+
+        private void CkbSquid_CheckedChanged(object sender, EventArgs e)
+        //I've done this so it acts as some sort of Data Validation
+        {   //if the checkbox is checked, it will add Squid to the listbox
+            if (ckbSquid.Checked == true)
+            {
+                lstOrderreveiw.Items.Add("Squid");
+            }
+            //if the checkbox is unchecked, it will remove the text above if it is present in the listbox
+            else
+            {
+                lstOrderreveiw.Items.Remove("Squid");
+            }
+        }
+
+        private void CkbHam_CheckedChanged(object sender, EventArgs e)
+        //I've done this so it acts as some sort of Data Validation
+        {   //if the checkbox is checked, it will add Ham to the listbox
+            if (ckbHam.Checked == true)
+            {
+                lstOrderreveiw.Items.Add("Ham");
+            }
+            //if the checkbox is unchecked, it will remove the text above if it is present in the listbox
+            else
+            {
+                lstOrderreveiw.Items.Remove("Ham");
+            }
+        }
+
+        private void CkbOlives_CheckedChanged(object sender, EventArgs e)
+        //I've done this so it acts as some sort of Data Validation
+        {   //if the checkbox is checked, it will add Olives to the listbox
+            if (ckbOlives.Checked == true)
+            {
+                lstOrderreveiw.Items.Add("Black Olives");
+            }
+            //if the checkbox is unchecked, it will remove the text above if it is present in the listbox
+            else
+            {
+                lstOrderreveiw.Items.Remove("Black Olives");
+            }
+        }
+
+        private void CkbPepperoni_CheckedChanged(object sender, EventArgs e)
+        //I've done this so it acts as some sort of Data Validation
+        {   //if the checkbox is checked, it will add Pepperoni to the listbox
+            if (ckbPepperoni.Checked == true)
+            {
+                lstOrderreveiw.Items.Add("Pepperoni");
+            }
+            //if the checkbox is unchecked, it will remove the text above if it is present in the listbox
+            else
+            {
+                lstOrderreveiw.Items.Remove("Pepperoni");
+            }
+        }
+
+        private void CkbPufferfish_CheckedChanged(object sender, EventArgs e)
+        //I've done this so it acts as some sort of Data Validation
+        {   //if the checkbox is checked, it will add Pufferfish to the listbox
+            if (ckbPufferfish.Checked == true)
+            {
+                lstOrderreveiw.Items.Add("Pufferfish");
+            }
+            //if the checkbox is unchecked, it will remove the text above if it is present in the listbox
+            else
+            {
+                lstOrderreveiw.Items.Remove("Pufferfish");
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            lblPrice.Text = ("Total Price: £15");
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            lstOrderreveiw.Items.RemoveAt(lstOrderreveiw.SelectedIndex);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lstOrderreveiw.Items.Add(cboStarter.SelectedItem);
+            lstOrderreveiw.Items.Add(cboBase.SelectedItem);
+            lstOrderreveiw.Items.Add(cboSweet.SelectedItem);
+
+        }
+    }
+}
